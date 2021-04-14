acee8957abcaadbbc8a301705b63d98ebef5555c
Kate Bryan <katebryan31@gmail.com>
2021-04-14 17:50:39 +0100

    chore: added discount variables

---
+++ src/data/discount.js
@@ -0,0 +1,6 @@
+// Set as variables for reusability - if the requirements change, update happens in one clear place.
+
+const PERCENTAGE = 10;
+const THRESHOLD = 60;
+
+module.exports = { PERCENTAGE, THRESHOLD };
