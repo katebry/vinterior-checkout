72698d573dfe91cf53d7a103ceced14ff439a70f
Kate Bryan <katebryan31@gmail.com>
2021-04-14 17:30:40 +0100

    feat: added totalPriceReturn func and unit ests

--- src/utils/totalCost.js
+++ src/utils/totalCost.js
@@ -1,14 +1,22 @@
 // finds and returns the quantity of each product in the basket, dependent on barcode.
-const productsInBasket = (basket) => {
-  if (basket.length <= 0) return false
+const quantityOfProductsInBasket = (basket) => {
+  if (basket.length <= 0) return false;
 
   const quantityOfProducts = basket.reduce((obj, product) => {
     obj[product] = ++obj[product] || 1;
     return obj;
   }, {});
-  
-  return quantityOfProducts
-  
+
+  return quantityOfProducts;
 };
 
-module.exports = { productsInBasket };
+// matches the barcode/ corresponding product and returns the combined price.
+const totalPriceReturn = (basket, products) => {
+  const combinedPrice = basket.map((item) => products[item].price).reduce((result, item) => {
+    return result + item;
+  }, 0);
+
+  return combinedPrice.toFixed(2);
+};
+
+module.exports = { quantityOfProductsInBasket, totalPriceReturn };
--- tests/totalCost.test.js
+++ tests/totalCost.test.js
@@ -1,25 +1,50 @@
-const { productsInBasket } = require("../src/utils/totalCost");
+const {
+  quantityOfProductsInBasket,
+  totalPriceReturn,
+} = require("../src/utils/totalCost");
+const products = require("../src/data/products");
 const chai = require("chai");
 const expect = chai.expect;
 
-describe("productsInBasket", () => {
+describe("quantityOfProductsInBasket", () => {
   it("If no products are in the basket, the function returns false", () => {
     const basket = [];
-    expect(productsInBasket(basket)).to.be.false;
+    expect(quantityOfProductsInBasket(basket)).to.be.false;
   });
 
   it("If one product is in the basket, the function returns the quantity of the item", () => {
     const basket = ["001"];
-    expect(productsInBasket(basket)).to.eql({ "001": 1 });
+    expect(quantityOfProductsInBasket(basket)).to.eql({ "001": 1 });
   });
 
   it("If two products with the same code are in the basket, the function returns the combined quantity", () => {
     const basket = ["001", "001"];
-    expect(productsInBasket(basket)).to.eql({ "001": 2 });
+    expect(quantityOfProductsInBasket(basket)).to.eql({ "001": 2 });
   });
 
   it("If products are in the basket, the function returns the quantity of each product", () => {
     const basket = ["001", "002", "001", "001", "003"];
-    expect(productsInBasket(basket)).to.eql({ "001": 3, "002": 1, "003": 1 });
+    expect(quantityOfProductsInBasket(basket)).to.eql({
+      "001": 3,
+      "002": 1,
+      "003": 1,
+    });
+  });
+});
+
+describe("totalPriceReturn ", () => {
+  it("If a product is in the basket, the price is returned", () => {
+    const basket = ["001"];
+    expect(totalPriceReturn(basket, products)).to.be.equal("9.25");
+  });
+
+  it("If two products with the same code are in the basket, the combined price is returned", () => {
+    const basket = ["001", "001"];
+    expect(totalPriceReturn(basket, products)).to.be.equal("18.50");
+  });
+
+  it("If multiple products are in the basket, the combined price is returned", () => {
+    const basket = ["001", "001", "002"];
+    expect(totalPriceReturn(basket, products)).to.be.equal("63.50");
   });
 });
